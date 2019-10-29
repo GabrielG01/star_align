5d220d07d98e20534995f35f8be5322e9e82ac03
GabrielG01 <gabrielprovo01@gmail.com>
2019-10-29 13:51:55 -0600

    Updated header styles

--- src/components/header.js
+++ src/components/header.js
@@ -5,11 +5,6 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <div className="header_home">
-          <NavLink exact to="/">
-            Home
-          </NavLink>
-        </div>
         <div className="header_art_posts">
           <NavLink exact to="/art_posts">
             Posts
@@ -18,6 +13,11 @@ class Header extends Component {
         <div className="header_create_post">
           <NavLink exact to="/create_post">
             Create Post
+          </NavLink>
+        </div>
+        <div className="header_home">
+          <NavLink exact to="/">
+            Home
           </NavLink>
         </div>
       </div>
