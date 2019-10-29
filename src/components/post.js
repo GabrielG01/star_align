ee569483aa1c06e1761e99af8240a65db077b8cb
GabrielG01 <gabrielprovo01@gmail.com>
2019-10-29 14:59:16 -0600

    Added placeholder images, styling out posts

--- src/components/post.js
+++ src/components/post.js
@@ -4,12 +4,16 @@ class Post extends Component {
   render() {
     return (
       <div className="post">
-        <div className="post_image"></div>
-        <div className="post_title">
-          <h1>epic title</h1>
+        <div className="post_image">
+          <h1>Image goes here</h1>
         </div>
-        <div className="post_description">
-          <p>Supa supa awesome description for epic awesome art piece</p>
+        <div className="post_text_fields">
+          <div className="post_title">
+            <h1>epic title</h1>
+          </div>
+          <div className="post_description">
+            <p>Supa supa awesome description for epic awesome art piece</p>
+          </div>
         </div>
       </div>
     );
